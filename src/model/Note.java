9f2c705de64d76468a0ff0d722dfbff2e6b9a94f
mcb <kurdemol94@gmail.com>
2017-04-26 00:28:47 +0200

    First commit

---
+++ src/model/Note.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Note {
+	String name;
+	String content;
+	String []tags;
+	
+	public Note(String name, String content, String[] tags) {
+		super();
+		this.name = name;
+		this.content = content;
+		this.tags = tags;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String[] getTags() {
+		return tags;
+	}
+	public void setTags(String[] tags) {
+		this.tags = tags;
+	}
+	
+	
+
+}
